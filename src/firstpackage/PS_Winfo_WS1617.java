0ebd84409b507e8df0423f8021f340bf7ed50587
sezer <sezer.gueven@stud.uni-regensburg.de>
2016-11-14 14:09:49 +0100

    Fisrt Commit

---
+++ src/firstpackage/PS_Winfo_WS1617.java
@@ -0,0 +1,43 @@
+package firstpackage;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PS_Winfo_WS1617 {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PS_Winfo_WS1617 window = new PS_Winfo_WS1617();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PS_Winfo_WS1617() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
